1e5f33792fdf10db543c4166ba386ab25e2a13c7
juny18 <93839402+juny18@users.noreply.github.com>
2022-02-21 17:54:15 -0800

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("CUNIFON", 700,150)
+fill("lightblue")
+text("Perfoms a particular task !!", 700,170)  
+
+
+fill("white")
+textSize(15)
+text("EDATEDOSBAS ", 100,370)
+fill("lightblue")
+text(" Stores all information !!", 100,390)
+
+}
